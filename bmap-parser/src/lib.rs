f906cee1c317eaf9faf0b308ecf528231259f9e3
Richard Rondu <rondu.richard@lainwir3d.net>
2024-06-07 10:39:29 +0100

    Copy-gpt: hack own backup header to speed up operation (by a lot)
    seeking through compressed wic file to backup gpt header at "end of disk" was too slow,
    this prevents us from doing this by making our own gpt backup header
    from the one read at start of disk.
    
    This way, we only read / seek through the first kB

--- bmap-parser/src/lib.rs
+++ bmap-parser/src/lib.rs
@@ -7,6 +7,7 @@ use futures::io::{AsyncRead, AsyncReadExt, AsyncSeek, AsyncSeekExt, AsyncWrite, 
 use futures::TryFutureExt;
 use sha2::{Digest, Sha256};
 use thiserror::Error;
+use crc::{Crc};
 
 use std::io::Result as IOResult;
 use std::io::{Read, Seek, SeekFrom, Write};
@@ -343,37 +344,38 @@ where
 {
     let sector_size = u64::from(gpt::disk::DEFAULT_SECTOR_SIZE);
 
-    let mut ranges: VecDeque<Range<u64>> = VecDeque::new();
-    
-    // copy protective MBR LBA
-    ranges.push_back(0..(1*sector_size));
-
-    // copy from gpt header start LBA to first usable LBA, indistinctive of partition start LBA
-    ranges.push_back((src_hdr.current_lba*sector_size)..(src_hdr.first_usable*sector_size));
-
-    // copy from gpt backup partitions table (last_usable + 1) LBA to backup header LBA (inclusive, so +1)
-    ranges.push_back(((src_hdr.last_usable+1)*sector_size)..((src_hdr.backup_lba+1)*sector_size));
-
-    println!("{:#?}", ranges);
-
-    let mut hasher = match map.checksum_type() {
-        HashType::Sha256 => Sha256::new(),
-    };
-
+    // main read buffer
     let mut v = Vec::new();
-    // TODO benchmark a reasonable size for this
     v.resize(8 * 1024 * 1024, 0);
-
     let buf = v.as_mut_slice();
+
+    // intermediary data holder (to be able to reconstitute backup gpt header)
+    let mut mbr_v: Vec<u8> = Vec::new();
+    let mut header_v: Vec<u8> = Vec::new();
+    let mut parts_v: Vec<u8> = Vec::new();
+
+    // holder for ranges to copy
+    let mut ranges: VecDeque<(&mut Vec<u8>, Range<u64>)> = VecDeque::new();
+
+    // copy protective MBR LBA
+    ranges.push_back((&mut mbr_v, 0..(1*sector_size)));
+
+    // copy gpt header
+    ranges.push_back((&mut header_v, (src_hdr.current_lba*sector_size)..((src_hdr.current_lba+1)*sector_size)));
+
+    // copy gpt partitions table (_usable + 1) LBA to backup header LBA (inclusive, so +1)
+    ranges.push_back((&mut parts_v, ((src_hdr.part_start)*sector_size)..((src_hdr.first_usable)*sector_size)));
+
+    //println!("Ranges to copy: {:#?}", ranges);
+
     let mut position = 0;
     for range in map.block_map() {
         let source_range = range.offset()..(range.offset() + range.length());
-        
+
         //println!("New source range {:#?}", source_range);
 
         // move to start of source block
         let mut forward = source_range.start - position;
-        
         if forward > 0 {
             input.seek_forward(forward).map_err(CopyError::ReadError)?;
             output
@@ -388,24 +390,25 @@ where
             break;
         }
 
-        let mut r = ranges.pop_front().unwrap();
-        
+        let mut pair = ranges.pop_front().unwrap();
+        let mut r = pair.1;
+
         loop {
             //println!("ranges {:#?}", ranges);
             //println!("{:#?}..{:#?} ->  {:#?}", source_range.start, source_range.end, r);
 
             // this one should never happen as blocks are in order
             if source_range.start > r.end {
-                println!("Source block went past GPT block!!");
+                //println!("Source block went past GPT block!!");
                 return Err(CopyError::GPTCopyError);
             }
 
             if (source_range.start < r.start) && (source_range.end < r.start) {
                 //println!("Block out of range!!");
-                ranges.push_front(r);
+                ranges.push_front((pair.0, r));
                 break; // next source block
             }
-            
+
             /*
             if (source_range.start >= r.start) && (source_range.end <= r.end) {
                 println!("Block fully inside destination partition.");
@@ -440,18 +443,20 @@ where
                 if r == 0 {
                     return Err(CopyError::UnexpectedEof);
                 }
-                hasher.update(&buf[0..r]);
                 output
                     .write_all(&buf[0..r])
                     .map_err(CopyError::WriteError)?;
                 left -= r;
+
+                // also store for later
+                pair.0.extend_from_slice(&buf[0..r]);
 
                 position += r as u64;
             }
 
             if r.end > source_range.end {
                 //println!("GPT block finish later than block. Inserting new GPT block at beginning");
-                ranges.push_front((source_range.end)..(r.end));
+                ranges.push_front((pair.0, (source_range.end)..(r.end)));
                 break;
             }
 
@@ -462,7 +467,8 @@ where
                 }
 
                 //println!("Data left in source block, move to next GPT block");
-                r = ranges.pop_front().unwrap();
+                pair = ranges.pop_front().unwrap();
+                r = pair.1;
                 continue;
             }
 
@@ -470,6 +476,94 @@ where
         }
     }
 
+    /*
+     * Starting from this point, GPT main header + parts have been copied, now we need to
+     * reconstruct backup gpt parts + header
+     */
+
+    //println!("mbr: {:#?}", mbr_v.len());
+    //println!("header: {:#?}", header_v.len());
+    //println!("parts: {:#?}", parts_v.len());
+
+    // check that our gpt data pointers are correct
+    if (src_hdr.last_usable + 1 + parts_v.len() as u64 / sector_size) != src_hdr.backup_lba {
+        return Err(CopyError::GPTCopyError);
+    }
+
+    // move to start of backup parts
+    let forward = (src_hdr.last_usable + 1) * sector_size - position;
+    if forward > 0 {
+        output
+            .seek_forward(forward)
+            .map_err(CopyError::WriteError)?;
+        position += forward;
+        //println!("Moving forward to {:#?} (+{:#?})", position, forward);
+    }
+    
+    // write parts, those do not change from main parts
+    output
+        .write_all(&parts_v)
+        .map_err(CopyError::WriteError)?;
+
+    /* 
+     * For backup header, we need to:
+     *     - Invert main header current lba and backup lba
+     *     - Point parts pointer to backup parts
+     *     - Reconstruct its crc32 because of the above changes
+     */
+    let mut backup_hdr = src_hdr.clone();
+    backup_hdr.current_lba = src_hdr.backup_lba;
+    backup_hdr.backup_lba = src_hdr.current_lba;
+    backup_hdr.part_start = src_hdr.last_usable + 1;
+
+    let intermediate_bytes = gptheader_as_bytes(&backup_hdr, false).unwrap();
+    backup_hdr.crc32 = calculate_crc32(&intermediate_bytes);
+
+    let mut finalbytes = gptheader_as_bytes(&backup_hdr, true).unwrap();
+    finalbytes.resize(header_v.len(), 0); // make it the same size
+
+    // finally write to disk
+    output
+        .write_all(&finalbytes)
+        .map_err(CopyError::WriteError)?;
+
     Ok(())
 }
 
+fn gptheader_as_bytes(hdr: &gpt::header::Header, use_crc32: bool
+        ) -> Result<Vec<u8>, std::io::Error> {
+    let mut buff: Vec<u8> = Vec::new();
+    let disk_guid_fields = hdr.disk_guid.as_fields();
+
+    std::io::Write::write_all(&mut buff, hdr.signature.as_bytes())?;
+    std::io::Write::write_all(&mut buff, &hdr.revision.to_le_bytes())?;
+    std::io::Write::write_all(&mut buff, &hdr.header_size_le.to_le_bytes())?;
+
+    if use_crc32 {
+        std::io::Write::write_all(&mut buff, &hdr.crc32.to_le_bytes())?;
+    } else {
+        std::io::Write::write_all(&mut buff, &[0_u8; 4])?;
+    }
+
+    std::io::Write::write_all(&mut buff, &[0; 4])?;
+    std::io::Write::write_all(&mut buff, &hdr.current_lba.to_le_bytes())?;
+    std::io::Write::write_all(&mut buff, &hdr.backup_lba.to_le_bytes())?;
+    std::io::Write::write_all(&mut buff, &hdr.first_usable.to_le_bytes())?;
+    std::io::Write::write_all(&mut buff, &hdr.last_usable.to_le_bytes())?;
+    std::io::Write::write_all(&mut buff, &disk_guid_fields.0.to_le_bytes())?;
+    std::io::Write::write_all(&mut buff, &disk_guid_fields.1.to_le_bytes())?;
+    std::io::Write::write_all(&mut buff, &disk_guid_fields.2.to_le_bytes())?;
+    std::io::Write::write_all(&mut buff, disk_guid_fields.3)?;
+    std::io::Write::write_all(&mut buff, &hdr.part_start.to_le_bytes())?;
+    std::io::Write::write_all(&mut buff, &hdr.num_parts.to_le_bytes())?;
+    std::io::Write::write_all(&mut buff, &hdr.part_size.to_le_bytes())?;
+    std::io::Write::write_all(&mut buff, &hdr.crc32_parts.to_le_bytes())?;
+    Ok(buff)
+}
+
+const CRC_32: Crc<u32> = Crc::<u32>::new(&crc::CRC_32_ISO_HDLC);
+fn calculate_crc32(b: &[u8]) -> u32 {
+    let mut digest = CRC_32.digest();
+    digest.update(b);
+    digest.finalize()
+}
